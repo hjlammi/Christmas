0f723bb7fad062690908588e5c2ea993dab3e44f
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2017-12-31 21:20:17 +0200

    Add comments and move images to image folder

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
+// The scene is rendered with camera.
 var scene = new THREE.Scene();
+// The first attribute is the field of view in degrees, the second is the aspect ratio,
+// the third and fourth are near and far clipping plane.
 var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+// Create renderer and set the size in which we want the app to be rendered.
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// Updating the app when resized.
 window.addEventListener('resize', function () {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -16,62 +21,63 @@ var loader = new THREE.TextureLoader();
 loader.crossOrigin = '';
 var texture1 = loader.load('0156337a.png');
 
+
+// Christmas balls.
 var geometry1 = new THREE.SphereGeometry(1.2, 32, 32);
 // immediately use the texture for material creation
 var material1 = new THREE.MeshPhongMaterial( { map: texture1, shininess: 150 } );
 var mesh1 = new THREE.Mesh(geometry1, material1);
 mesh1.position.set(-3, 1, 0);
 
-var texture2 = loader.load('d80b27ab.png');
+var texture2 = loader.load('img/d80b27ab.png');
 var geometry2 = new THREE.SphereGeometry(2, 32, 32);
 var material2 = new THREE.MeshPhongMaterial({map: texture2, shininess: 150});
 var mesh2 = new THREE.Mesh(geometry2, material2);
 mesh2.position.set(3, 2, 0);
 
-var texture3 = loader.load('76c4481a.png');
+var texture3 = loader.load('img/76c4481a.png');
 var geometry3 = new THREE.SphereGeometry(1.3, 32, 32);
 var material3 = new THREE.MeshPhongMaterial({map: texture3, shininess: 150});
 var mesh3 = new THREE.Mesh(geometry3, material3);
 mesh3.position.set(0.2, -3.2, -1);
 
-var texture4 = loader.load('b1a749af.png');
+var texture4 = loader.load('img/b1a749af.png');
 var geometry4 = new THREE.SphereGeometry(1.8, 32, 32);
 var material4 = new THREE.MeshPhongMaterial({map: texture4, shininess: 150});
 var mesh4 = new THREE.Mesh(geometry4, material4);
 mesh4.position.set(-7.2, 4.2, 1);
 
-var texture5 = loader.load('435d6d93.png');
+var texture5 = loader.load('img/435d6d93.png');
 var geometry5 = new THREE.SphereGeometry(1, 32, 32);
 var material5 = new THREE.MeshPhongMaterial({map: texture5, shininess: 150});
 var mesh5 = new THREE.Mesh(geometry5, material5);
 mesh5.position.set(-5.2, -4.2, 1);
 
-var texture6 = loader.load('b1a749af.png');
+var texture6 = loader.load('img/b1a749af.png');
 var geometry6 = new THREE.SphereGeometry(1.4, 32, 32);
 var material6 = new THREE.MeshPhongMaterial({map: texture6, shininess: 150});
 var mesh6 = new THREE.Mesh(geometry6, material6);
 mesh6.position.set(10.5, -3.2, 1);
 
-var texture7 = loader.load('435d6d93.png');
+var texture7 = loader.load('img/435d6d93.png');
 var geometry7 = new THREE.SphereGeometry(1.9, 32, 32);
 var material7 = new THREE.MeshPhongMaterial({map: texture7, shininess: 150});
 var mesh7 = new THREE.Mesh(geometry7, material7);
 mesh7.position.set(7.5, 3.5, 1);
 
-var texture8 = loader.load('165cd273.png');
+var texture8 = loader.load('img/165cd273.png');
 var geometry8 = new THREE.SphereGeometry(1.4, 32, 32);
 var material8 = new THREE.MeshPhongMaterial({map: texture8, shininess: 150});
 var mesh8 = new THREE.Mesh(geometry8, material8);
 mesh8.position.set(-10.7, -0.4, 1);
 
-var texture9 = loader.load('165cd273.png');
+var texture9 = loader.load('img/165cd273.png');
 var geometry9 = new THREE.SphereGeometry(1.1, 32, 32);
 var material9 = new THREE.MeshPhongMaterial({map: texture9, shininess: 150});
 var mesh9 = new THREE.Mesh(geometry9, material9);
 mesh9.position.set(6.4, -3.8, 1);
 
-//create a group and add the two cubes
-//These cubes can now be rotated / scaled etc as a group
+//create a group and add the spheres.
 var group = new THREE.Group();
 group.add( mesh1 );
 group.add( mesh2 );
@@ -95,8 +101,9 @@ scene.add(ambientLight);
 
 camera.position.z = 23;
 
+// Create snowflakes.
 var snowflakeGeometry = new THREE.Geometry();
-var snowflake = new THREE.TextureLoader().load('snowflake.png');
+var snowflake = new THREE.TextureLoader().load('img/snowflake.png');
 
 for (i = 0; i < 2000; i++) {
   var snowflake1 = new THREE.Vector3();
